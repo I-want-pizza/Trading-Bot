391eaf313f21f8cc3dbfb53bf7590d3d80fc3ad6
romanpepper <wupsen1703@mail.ru>
2024-07-03 23:53:12 +0300

    added Roma's code

--- src/main/java/ru/trading/Main.java
+++ src/main/java/ru/trading/Main.java
@@ -18,25 +18,28 @@ import static ru.trading.data.API.getHistoricalData;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-//        String symbol = "NVDA"; // Apple on US exchange
-//        Calendar from = Calendar.getInstance();
-//        Calendar to = Calendar.getInstance();
-//
-//        // Example: Fetch data from the last 200 days (no holidays)
-//        from.add(Calendar.DAY_OF_YEAR, -360);
-//
-//        StockData historicalData = getHistoricalData(symbol, from, to);
-//        if (historicalData.isEmpty()) {
-//            System.out.println("No data found.");
-//        } else {
-//            for (Quotation quotation : historicalData) {
-//                System.out.println(quotation.getCandle() + " " + quotation.getDate());
-//            }
-//        }
-//        TechAnalis techAnalis = new TechAnalis(historicalData);
-//        float[] points = techAnalis.entryPoints();
-//        System.out.println(Arrays.toString(points));
+        //Твое новое
+        String symbol = "NVDA"; // Apple on US exchange
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
 
+        // Example: Fetch data from the last 200 days (no holidays)
+        from.add(Calendar.DAY_OF_YEAR, -360);
+
+        StockData historicalData = getHistoricalData(symbol, from, to);
+        if (historicalData.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            for (Quotation quotation : historicalData) {
+                System.out.println(quotation.getCandle() + " " + quotation.getDate());
+            }
+        }
+        TechAnalis techAnalis = new TechAnalis(historicalData);
+        float[] points = techAnalis.entryPoints();
+        System.out.println(Arrays.toString(points));
+
+
+        // Моё старое
         Calendar start = Calendar.getInstance();
         Calendar end = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat(Constants.timePattern);
